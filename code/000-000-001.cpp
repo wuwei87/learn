056c6d05e68e097ff88e14aa5ef5c8278c0664ab
wuwei87 <1249522631@qq.com>
2021-03-17 11:40:10 +0800

    add code

---
+++ code/000-000-001.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(int argc,char *argv[]){
+	cout<<"zhangxioayu"<<endl;
+	for(int i = 0;i<10;i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}
+
